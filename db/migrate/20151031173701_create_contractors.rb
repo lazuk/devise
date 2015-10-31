dc6e9da6f4818c5c931664cb5b2402c40d546d82
Prelang Builder <builder@prelang.com>
2015-10-31 17:37:01 +0000

    Generated model Contractor with migration

---
+++ db/migrate/20151031173701_create_contractors.rb
@@ -0,0 +1,11 @@
+class CreateContractors < ActiveRecord::Migration
+  def change
+    create_table :contractors do |t|
+      t.string :name
+      t.string :fullname
+      t.json :data
+
+      t.timestamps
+    end
+  end
+end
